0e0c550de350bb2775220a912e664a9068d765c0
Aaron Pittenger <pittenger.aaron@gmail.com>
2015-07-08 22:06:56 -0400

    Updated the nav bar to properly add sub-headings automatically

--- blog.md
+++ blog.md
@@ -5,7 +5,7 @@ title: Blog
 
 {% for post in site.posts %}
   <div class="post">
-    <h2>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ post.url }}">{{ post.title }}</a></h2>
+    <h1>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ post.url }}">{{ post.title }}</a></h1>
     {{ post.content }}
     <small>Written by {{ post.author }}</small>
   </div>
--- javascripts/main.js
+++ javascripts/main.js
@@ -13,22 +13,29 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
+
+  $("section h1").each(function(){
+
+    aObj = document.getElementById('nav').getElementsByTagName('a');
+    for(i=0;i<aObj.length;i++) {
+      if(document.location.href == aObj[i].href){
+        aObj[i].className = 'active'
+        $("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>").insertAfter(aObj[i].parentElement);
+      }
+    }
   });
-  
+
+
   $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
-    event.preventDefault();    
+    event.preventDefault();
   });
-  
+
   sectionHeight();
-  
+
   $('img').load(sectionHeight);
 });
 
